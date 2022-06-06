678f7bd94f3d61235b2147a01218358b69c189a7
Carlos-S-Rodrigues <carlos.schuenck.rodrigues@gmail.com>
2022-06-06 16:00:08 -0300

    basicJS_TimeOfTheDay

---
+++ script.js
@@ -0,0 +1,18 @@
+function loading() {
+  var msg = window.document.getElementById('message')
+  var img = window.document.getElementById('image')
+  var date = new Date()
+  var hour = date.getHours()
+  var min = date.getMinutes()
+  msg.innerHTML = `Agora são ${hour} horas e ${min} minutos`
+  if (hour >= 0 && hour < 12) {
+    img.src = 'img/img01.png'
+    document.body.style.background = '#abe329'
+  } else if (hour >= 12 && hour <= 18) {
+    img.src = 'img/img02.png'
+    document.body.style.background = '#e38329'
+  } else {
+    img.src = 'img/img03.png'
+    document.body.style.background = '#460d75'
+  }
+}
